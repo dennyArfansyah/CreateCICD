a4dabfda6dc37b8a906a8f6ab151d9fd991d6adc
dennyArfansyah <dennyarfansyah@gmail.com>
2024-12-22 19:47:59 +0700

    Initial Commit

---
+++ CreateCICD/CreateCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateCICDApp.swift
+//  CreateCICD
+//
+//  Created by Denny Arfansyah on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CreateCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
